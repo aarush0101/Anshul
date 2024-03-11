280d538bf1fa4744560a0a21be3714fbd4b5765b
Riyan <141508418+riyansarma76@users.noreply.github.com>
2024-03-11 15:28:39 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   bot.py
    	new file:   temp/log.log
    	new file:   utils/__pycache__/config.cpython-310.pyc
    	new file:   utils/__pycache__/logger.cpython-310.pyc
    	new file:   utils/bot.py
    	new file:   web/__pycache__/web.cpython-310.pyc
    	modified:   web/style.css

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ __version__ = '2.0'
 import os
 import discord
 from discord.ext import commands
+from git import Optional
 from utils.logger import getLogger, configure_logging
 import logging
 from utils.config import Config as cm
-from web import web
+import dotenv
 import asyncio
 from aiohttp import ClientSession
-
+from typing import Union
 
 logger = getLogger(__name__)
 
@@ -28,12 +29,12 @@ def _configure_logging(name):
     
     if log_level is None:
         log_level = logging.INFO
-        logger.warning("Invalid logging level set: %s.", level_text)
+        logger.warning("Invalid logging level set: %s." % (level_text,))
         logger.warning("Using default logging level: INFO.")
     else:
-        logger.info("Logging level: %s", level_text)
+        logger.info("Logging level: %s" % (level_text,))
 
-    logger.info("Log file: %s", name)
+    logger.info("Log file: %s" % (name,))
     configure_logging(name, log_level)
     logger.debug("Successfully configured logging.")
 
@@ -42,29 +43,166 @@ def _configure_logging(name):
 
 class YourMom(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=cm("Prefix"), tree_cls=MainTree, description="Main client for anshul's YourMom bot",intents=discord.Intents.all())
+        super().__init__(command_prefix=cm("Prefix"), description="Main client for anshul's YourMom bot",intents=discord.Intents.all())
+        self.started = False
+        self.config = cm
+
+    @property
+    def owners(self):
+        """
+        Return list of bot owner ID(s)
+        """
+        return self.config("Owners")
+    @property
+    def copyright(self):
+        """
+        Return the author of this project
+        """
+        return self.config("Copyright")
+    @property
+    def logging_(self) -> in:
+        """
+        Return log channel ID for logging
+        """
+        return self.config("Log_Channel_ID")
     
+    @property
+    def gifted_to(self) -> str:
+        """
+        Returns to whom is this licensed to
+        """
+        return self.config("Owner_Name")
+
+    @property
+    def hosting_method(self) -> str:
+        """
+        Returns on what is this currently running
+        """
+        if 'REPLIT_DB_URL' in dotenv.dotenv_values:
+            return "REPLIT:("
+        import platform
+        system = platform.system().lower()
+        if system:
+            if system.lower() == 'windows':
+                if platform.architecture()[0] == '32bit':
+                    return "WINDOWS 32 BIT"
+                elif platform.architecture()[0] == '64bit':
+                    return "WINDOWS 64 BIT"
+                else:
+                    return "WINDOWS"
+            if system.lower() == 'linux':
+                return "LINUX"
+            if system.lower() == 'darwin':
+                return "MAC OS"
+            if system.lower() == 'atheos':
+                return "ATHE OS"
+            if system.lower() == 'riscos':
+                return "RISC OS"
+            if system.lower() == 'os/2':
+                return "OS/2"
+            if system.lower() == 'java':
+                return "JAVA VIRTUAL MACHINE"
+            if system.lower() == 'java freebsd':
+                return "JAVA FREEBSD"
+
+        return "Couldn't detect system"
+    
+    @property
+    def cogs_dir(self) -> str:
+        """
+        Returns the cog directory where commands are located
+        """
+        root = os.path.join(os.path.dirname(__file__), 'bot.py')
+        cogs_dir = os.path.join(root, 'commands')
+        return cogs_dir
+    
+    
+    async def load_extension(self) -> None:
+        """
+        Load the commands into the bot for usage.
+        """
+        await self.wait_until_ready()
+        dir = self.cogs_dir
+        
+        for cog in dir:
+            if cog in self.loaded_cog:
+                continue
+            logger.debug("Loading %s cog..." % (dir,))
+            try:
+                await self.load_extension(dir)
+                logger.debug("Loaded cog: %s" % (dir,))
+            except Exception:
+                logger.error("Unable to load cog: %s" % (dir,))
+        logger.debug("dbg")
+
+    async def get_owners(self) -> str:
+        """
+        Returns str of owners the bot currently has listed in its config
+        """
+        ids_ = self.owners
+        owners = ", ".join(
+            getattr(self.get_user_(owner_id), "name", str(owner_id)) for owner_id in ids_
+        )
+        return owners
+    
+    async def get_user_(self, id: int) -> discord.User:
+        """
+        Similar to get_user of discord module but this tries getting the user from the cache and falls back to making
+        an API call if they're not found in the cache.
+        """
+        return await self.get_user(id) or await self.fetch_user(id)
+    
+    async def get_channel_(self, id: int) -> Optional[Union[discord.TextChannel, discord.ForumChannel, discord.VoiceChannel, discord.StageChannel, discord.CategoryChannel, discord.DMChannel, discord.channel.VocalGuildChannel]]:
+        """
+        Similar to get_channel of discord module but this tries to get the channel from the cache and if not found, try to get from API calls.
+        """
+        return await self.get_channel(id) or await self.fetch_channel(id)
+        
+    async def checker(self) -> bool:
+        """
+        Checks if everything is perfect before starting, this is called in on_ready event.
+        """
+        pass
+        # TODO: MAKE IT RN
+    
+    @property
+    def invite(self) -> str:
+        """
+        Returns the bot invite link. Explicitly generated through discord.utils
+        """
+        permissions = discord.PermissionOverwrite(
+            administrator=True
+        )
+        invite_link = discord.utils.oauth_url(client_id=self.user.id, permissions=permissions)
+        return invite_link
+    
+    @property
+    def guild_invite(self):
+
+
+
+
+
     async def on_ready(self):
         logger.info("Starting")
-        await check()
+        # await check()
 
     def run(self):
         async def runner():
             async with self:
-                self._connected = asyncio.Event()
                 self.session = ClientSession(loop=self.loop)
 
                 try:
                     await self.start(self.token)
+                    
                 except discord.PrivilegedIntentsRequired:
-                    logger.critical(
-                        "Privileged intents are not explicitly granted in the discord developers dashboard."
-                    )
+                    logger.critical("Privileged intents are not explicitly granted in the discord developers dashboard.")
+
                 except discord.LoginFailure:
                     logger.critical("Invalid token")
-                    raise RuntimeError("Invalid Token")
-                except Exception:
-                    logger.critical("Fatal exception")
+
+                except Exception as e:
+                    logger.critical("Fatal exception\n%s" % (e,))
                 finally:
                     if not self.is_closed():
                         await self.close()
@@ -72,7 +210,7 @@ class YourMom(commands.Bot):
             async with self:
                 task_retriever = asyncio.all_tasks
                 loop = self.loop
-                tasks = {t for t in task_retriever() if not t.done() and t.get_coro() != cancel_tasks_coro}
+                tasks = {t for t in task_retriever() if not t.done() and t.get_coro() != cancel_tasks}
 
                 if not tasks:
                     return
@@ -89,7 +227,7 @@ class YourMom(commands.Bot):
                         if task.exception() is not None:
                             loop.call_exception_handler(
                                 {
-                                    "message": "Unhandled exception during Client.run shutdown.",
+                                    "message": "Unhandled exception during shutdown.",
                                     "exception": task.exception(),
                                     "task": task,
                                 }
@@ -105,8 +243,8 @@ class YourMom(commands.Bot):
             logger.info("Cleaning up tasks.")
 
             try:
-                cancel_tasks_coro = _cancel_tasks()
-                asyncio.run(cancel_tasks_coro)
+                cancel_tasks = _cancel_tasks()
+                asyncio.run(cancel_tasks)
             finally:
                 logger.info("Closing the event loop.")
 
@@ -118,7 +256,7 @@ def main():
     try:
         bot = YourMom()
         bot.remove_command('help')
-        bot.run(token=cm("Token"))
+        bot.run()
     except (discord.ClientException, discord.LoginFailure):
         logger.critical("Invalid token, get a valid one first.")
         raise RuntimeError("Invalid Token")
